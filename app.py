edc668835f4c24e02481cb28c67a634a1c31f7b2
avital.abramov <avital.abramov@tufin.com>
2024-08-12 14:46:44 +0300

    go back to flask only without blueprint

--- app.py
+++ app.py
@@ -1,31 +1,25 @@
-from flask import Flask, Blueprint, jsonify, request, render_template, send_from_directory
+from flask import Flask, jsonify, request, render_template, send_from_directory
 
-bp = Blueprint("routes", __name__, static_folder="static", template_folder="static")
+app = Flask(__name__, static_folder='static', template_folder='static')
 
-@bp.route("/")
+@app.route("/")
 def index():
     return render_template("index.html", title="Chatbot", favicon="favicon.ico")
 
-@bp.route("/favicon.ico")
+@app.route("/favicon.ico")
 def favicon():
     return send_from_directory("static", "favicon.ico")
 
-@bp.route("/assets/<path:path>")
+@app.route("/assets/<path:path>")
 def assets(path):
     return send_from_directory("static/assets", path)
 
-@bp.route("/conversation", methods=["POST"])
+@app.route("/conversation", methods=["POST"])
 def conversation():
     if not request.is_json:
         return jsonify({"error": "request must be json"}), 415
     request_json = request.get_json()
     return jsonify({"response": "Processed"})
 
-def create_app():
-    app = Flask(__name__, static_folder='static', template_folder='static')
-    app.register_blueprint(bp)
-    return app
-
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
